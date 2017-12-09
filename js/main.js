8014f8705898401a5983bd052e324a7ae6ccc532
Okeke Desmond <okekedesmond71@gmail.com>
2017-12-09 11:45:17 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -38,3 +38,15 @@ $('.homehome').click(function (event) {
   event.preventDefault();
   $('#home').scrollView();
 });
+
+function initMap() {
+        var uluru = {lat: 6.5243793, lng: 3.379205700000057};
+        var map = new google.maps.Map(document.getElementById('map_canvas'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
